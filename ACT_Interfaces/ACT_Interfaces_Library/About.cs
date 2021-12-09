98befc5ce02205910faa2f0f577beb26b5ec929d
IVolt <mark@inspiredvoltage.com>
2021-12-09 02:30:19 -0600

    Basic Information Passed Into It Initial Commit

---
+++ ACT_Interfaces/ACT_Interfaces_Library/About.cs
@@ -0,0 +1,11 @@
+﻿namespace ACT.Interfaces
+{
+    public static class About
+    {
+        public static string AboutMe()
+        {
+            string _tmpReturn = "{\"depends on\":\"\"}";
+            return _tmpReturn;
+        }
+    }
+}
